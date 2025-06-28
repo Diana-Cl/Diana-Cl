93eeecab7cad9da23a0308796479d79a21953665
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-06-28 22:56:23 +0330

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,28 +1,74 @@
 import { defineConfig } from 'vitepress'
+import footnote from 'markdown-it-footnote'
+import mathjax3 from 'markdown-it-mathjax3'
 
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
+// .vitepress/config.js
+export default {
   title: "Diana's Blog",
-  description: "Freedom to Dream",
+  description: 'My personal notes and articles.',
+  base: '/Diana-Cl/', 
+  lang: 'fa-IR',
+  dir: 'rtl',
+  cleanUrls: true,
+  ignoreDeadLinks: true,
+  head: [
+    ['link', { rel: 'icon', href: '/Diana-Cl/Diana-Cl/posts/favicon.ico' }]
+  ],
+
+  markdown: {
+    config: (md) => {
+      md.use(footnote)
+      md.use(mathjax3)
+    }
+  },
+
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
+    docFooter: {
+      prev: 'Previous page',
+      next: 'Next page'
+    },
+
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'All Posts',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Google Gemini', link: '/posts/Gemini' },
+          { text: 'REvil Ransomware', link: '/posts/REvil' },
+          { text: 'Windows Activation', link: '/posts/windows-activation' },
+          { text: 'AI Agents', link: '/posts/ai-agents' },
+          { text: 'Cloudflare Backend Selector', link: '/posts/cf-backend-selector' },
+          { text: 'Stable Diffusion', link: '/posts/Stable-Diffiusion' },
+          { text: 'Upgrade PHP on CentOS 7', link: '/posts/Upgrade-PHP-version-on-CentOS-7' }
         ]
       }
     ],
+    
+    lastUpdated: {
+      text: 'Last update:',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    
+    editLink: {
+      pattern: 'https://github.com/Diana-Cl/Diana-Cl/edit/main/posts/:path',
+      text: 'Edit this page on GitHub'
+    },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/Diana-Cl/Diana-Cl' },
+      { icon: 'telegram', link: 'https://t.me/F_NiREvil' }
+    ],
+
+    footer: {
+      message: 'Released under the REvil License.',
+      copyright: '© 2025 Dìana - All Rights Reserved.'
+    }
   }
-})
+}
