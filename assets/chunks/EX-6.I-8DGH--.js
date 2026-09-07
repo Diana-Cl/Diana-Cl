b89ba06bbc29f9965eeb82a0c93eacc368700f8d
Diana-Cl <135276904+Diana-Cl@users.noreply.github.com>
2026-09-07 19:51:38 +0400

    Updates

--- assets/chunks/EX-6.I-8DGH--.js
+++
@@ -1,1 +0,0 @@
-const n="/Diana-Cl/clients/EX-6.png";export{n as _};
--- assets/chunks/swimlanesDiagram-G3AALYLV.pIrdA8ZZ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.Bk0izgec.js";import{_ as a}from"../app.B-3s3njZ.js";import"./chunk-5VM5RSS4.xNuukvRb.js";import"./chunk-XXDRQBXY.oV4S0GM-.js";import"./chunk-VR4S4FIN.lzyRgnio.js";import"./chunk-32BRIVSS.uzeNwmGW.js";import"./channel.CfAjw6e_.js";import"./framework.BJi0JF_z.js";import"./theme.ylEREaU7.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
