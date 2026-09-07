b508116687d97724887b272c1ce4614b83ca0bf8
Diana <135276904+Diana-Cl@users.noreply.github.com>
2026-09-07 16:33:08 +0330

    Refactor enhanceApp method for better readability

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,37 +1,42 @@
-import DefaultTheme from 'vitepress/theme';
-import type { Theme } from 'vitepress';
-import { useRoute } from 'vitepress';
-import Ltr from './components/Ltr.vue';
-import CopyLink from './components/CopyLink.vue';
-import DocFooter from './components/DocFooter.vue';
-import PokerHand from './components/PokerHand.vue';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import CitationLink from './components/CitationLink.vue';
-import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { NolebaseInlineLinkPreview } from '@nolebase/vitepress-plugin-inline-link-preview/client';
-import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css';
-import 'viewerjs/dist/viewer.min.css';
-import './styles.css';
-import './custom.css';
-import './vars.css';
+
+import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+import { useRoute } from 'vitepress'
+
+import Ltr from './components/Ltr.vue'
+import CopyLink from './components/CopyLink.vue'
+import DocFooter from './components/DocFooter.vue'
+import PokerHand from './components/PokerHand.vue'
+import CitationLink from './components/CitationLink.vue'
+
+import imageViewer from 'vitepress-plugin-image-viewer'
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
+import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import { NolebaseInlineLinkPreview } from '@nolebase/vitepress-plugin-inline-link-preview/client'
+
+import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css'
+import 'viewerjs/dist/viewer.min.css'
+
+import './styles.css'
+import './custom.css'
+import './vars.css'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp?.(ctx);
 
-    ctx.app.component('vImageViewer', vImageViewer);
-    enhanceAppWithTabs(ctx.app);
-    ctx.app.component('NolebaseInlineLinkPreview', NolebaseInlineLinkPreview);
-    ctx.app.component('PokerHand', PokerHand);
-    ctx.app.component('CitationLink', CitationLink);
-    ctx.app.component('Ltr', Ltr);
-    ctx.app.component('CopyLink', CopyLink);
+  enhanceApp({ app }) {
+    app.component('vImageViewer', vImageViewer)
+    app.component('NolebaseInlineLinkPreview', NolebaseInlineLinkPreview)
+    app.component('PokerHand', PokerHand)
+    app.component('CitationLink', CitationLink)
+    app.component('Ltr', Ltr)
+    app.component('CopyLink', CopyLink)
+
+    enhanceAppWithTabs(app)
   },
-  setup() {
-    const route = useRoute();
-    imageViewer(route);
-  }
-} satisfies Theme;
 
+  setup() {
+    const route = useRoute()
+    imageViewer(route)
+  }
+} satisfies Theme
