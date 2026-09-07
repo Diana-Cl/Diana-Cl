301bdee776c70f04a90a2a14ecbd4c7da97bed9f
Diana <135276904+Diana-Cl@users.noreply.github.com>
2026-09-07 15:13:08 +0330

    Refactor imports and components in index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,19 +1,19 @@
 import DefaultTheme from 'vitepress/theme';
 import type { Theme } from 'vitepress';
 import { useRoute } from 'vitepress';
-import './styles.css';
-import './vars.css';
-import 'viewerjs/dist/viewer.min.css';
-import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css';
-import './custom.css';
 import Ltr from './components/Ltr.vue';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
-import { NolebaseInlineLinkPreview } from '@nolebase/vitepress-plugin-inline-link-preview/client';
+import CopyLink from './components/CopyLink.vue';
 import PokerHand from './components/PokerHand.vue';
+import imageViewer from 'vitepress-plugin-image-viewer';
 import CitationLink from './components/CitationLink.vue';
-
+import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { NolebaseInlineLinkPreview } from '@nolebase/vitepress-plugin-inline-link-preview/client';
+import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css';
+import 'viewerjs/dist/viewer.min.css';
+import './styles.css';
+import './custom.css';
+import './vars.css';
 
 export default {
   extends: DefaultTheme,
@@ -26,6 +26,7 @@ export default {
     ctx.app.component('PokerHand', PokerHand);
     ctx.app.component('CitationLink', CitationLink);
     ctx.app.component('Ltr', Ltr);
+    ctx.app.component('CopyLink', CopyLink);
   },
   setup() {
     const route = useRoute();
